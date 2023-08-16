aa397bc73c5dd0a1ce74b948845851f883c46e33
Jamal <sayedjamal.ahmadi@dci-student>
2023-08-16 17:00:45 +0200

    ABC

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  }
-  // base: "/atayee-electric/"
+  // server: {
+  //   port: 3000,
+  // }
+  base: "/atayee-electric/"
 });
